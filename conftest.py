77fe5e5a001fe1c7b37ab33ff30be0b3207be8cb
Ilya Tikhonov <absolute300@yandex.ru>
2026-06-02 20:08:48 +0300

    Test code and fix bugs

--- conftest.py
+++ conftest.py
@@ -1,16 +1,15 @@
 import pytest
 from selenium import webdriver
-from helper import OrderDataHelper
-from URL import MAIN_URL
+from selenium.webdriver.firefox.options import Options
 
-@pytest.fixture
+
+@pytest.fixture(scope="function")
 def driver():
-    driver=webdriver.Firefox()
-    driver.maximize_window()
-    driver.get(MAIN_URL)
+    firefox_options = Options()
+    firefox_options.add_argument("--width=1920")
+    firefox_options.add_argument("--height=1080")
+    firefox_options.set_preference("browser.privatebrowsing.autostart", True)
+    driver = webdriver.Firefox(options=firefox_options)
+    driver.get('https://qa-scooter.praktikum-services.ru/')
     yield driver
     driver.quit()
-
-@pytest.fixture
-def data():
-    return OrderDataHelper.generate_order_data()
